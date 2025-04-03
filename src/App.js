a2e5fd07216fe4ebdbde0bd8ce8fa0e267a9d9c4
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-03 12:41:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import {BrowserRouter as  Routes , Route} from 'react-router-dom'
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/global/Sidebar';
+import Appbar from './components/global/Appbar';
+import { CssBaseline , ThemeProvider } from '@mui/material';
+import { ColorModeContext , useMode } from './theme';
+
 
 function App() {
+
+  const [theme , colorMode] = useMode()
+
   return (
-
-      <div className="App">
-        <Sidebar />
-      </div>
-
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme} >
+        <CssBaseline />
+        <div className="App">
+          <Sidebar />
+          <Appbar />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
