efcc8e65403b43cff00280ee907c70a08e6892e6
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-06 11:30:18 +0300

    Add files via upload

---
+++ src/components/main/ButtonPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ButtonAsset from '../assets/ButtonAsset'
+
+const ButtonPage = () => {
+  return (
+    <div>
+        <ButtonAsset />
+    </div>
+  )
+}
+
+export default ButtonPage
