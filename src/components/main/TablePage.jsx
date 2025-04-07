83b3d95fcdf93a87c2d81bb4eddefa8859629830
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-07 19:22:57 +0300

    Add files via upload

---
+++ src/components/main/TablePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableAsset from '../assets/TableAsset'
+
+const TablePage = () => {
+  return (
+    <div>
+        <TableAsset title={'General table'}  content={'Name'} content1={'Calories'} />
+    </div>
+  )
+}
+
+export default TablePage
