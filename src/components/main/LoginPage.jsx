fbc273d4c2343d511e8068066cb2158f4186227a
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-08 18:36:55 +0300

    Add files via upload

---
+++ src/components/main/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginAsset from '../assets/LoginAsset'
+
+
+const LoginPage = () => {
+  return (
+    <div>
+        <LoginAsset/>
+    </div>
+  )
+}
+
+export default LoginPage
