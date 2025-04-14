eb7f3e2535715a18d1100120b83b250f2662cecf
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-04-14 12:09:37 +0300

    Add files via upload

---
+++ src/components/main/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginAsset from '../assets/LoginAsset'
+
+const RegisterPage = () => {
+  return (
+    <div>
+        <LoginAsset/>
+    </div>
+  )
+}
+
+export default RegisterPage
